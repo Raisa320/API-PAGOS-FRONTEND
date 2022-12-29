85c107ecdabe33d65de37a9084df2ca99c17804b
Paulo Véliz <pvelizs16@gmail.com>
2022-12-29 18:49:09 -0500

    put_servicios completo y update servicios.html

--- static/js/put_servicios.js
+++ static/js/put_servicios.js
@@ -1,5 +1,15 @@
 import { BASE_URL } from "./auth.js";
 
+function agregarServicios(serviciosList) {
+  const servicios = document.getElementById("servicio");
+  serviciosList.forEach((service) => {
+    const option = document.createElement("option");
+    option.value = service.id;
+    option.text = service.name;
+    servicios.appendChild(option);
+  });
+}
+
 let token = JSON.parse(localStorage.getItem("authTokens"));
 let servicesGet = async () => {
   let response = await fetch(BASE_URL + "api/v2/services/", {
@@ -9,115 +19,120 @@ let servicesGet = async () => {
       'Authorization': "Bearer " + token.access
     },
   });
-  console.log(response);
-  // let data = await response.json();
-  // if (response.status === 200) {
-  //   let { results } = data;
-  //   agregarServicios(results);
-  // } else {
-  //   console.error("Error en la carga de servicios.");
-  // }
+  let data = await response.json();
+
+  if (response.status === 200) {
+    let { results } = data;
+    agregarServicios(results);
+  } else {
+    console.error("Error en la carga de servicios.");
+  }
 };
 
 servicesGet();
 
+const selectedElement = document.getElementById('servicio');
+selectedElement.addEventListener('change', (event) => {
+  let id = event.target.value
+  servicesGetOne(id);
+});
 
+let servicesGetOne = async (id) => {
+  let response = await fetch(BASE_URL + "api/v2/services/"+id+'/', {
+    method: "GET",
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': "Bearer " + token.access
+    },
+  });
+  let data = await response.json();
 
-// // Put de Servicios
-// const formEdit = document.getElementById('form_edit');
-// const servicio = document.getElementById('servicio');
-// const nombre = document.getElementById('nombre_edit');
-// const prefijo = document.getElementById('prefijo_edit');
-// const urlLogo = document.getElementById('urlLogo_edit');
-// let msg = document.getElementById("msg");
-// let msg1 = document.getElementById("msg1");
-// let msg2 = document.getElementById("msg2");
+  if (response.status === 200) {
+    document.getElementById('nombre_edit').value = data.name;
+    document.getElementById('prefijo_edit').value = data.description;
+    document.getElementById('urlLogo_edit').value = data.logo;
+  } else {
+    document.getElementById('nombre_edit').value = '';
+    document.getElementById('prefijo_edit').value = '';
+    document.getElementById('urlLogo_edit').value = '';
+    console.error("Error en traer un servicio.");
+  }
+};
 
-// addEventListener('change', (event) =>{
-// });
+// Put de Servicios
+const formEdit = document.getElementById('form_edit');
+const servicio = document.getElementById('servicio');
+const nombre = document.getElementById('nombre_edit');
+const prefijo = document.getElementById('prefijo_edit');
+const urlLogo = document.getElementById('urlLogo_edit');
+let msg = document.getElementById("msg3");
+let msg1 = document.getElementById("msg4");
+let msg2 = document.getElementById("msg5");
 
-// formAdd.addEventListener('submit', (event) => {
-//     event.preventDefault();
-//     formValidation();
-// });
+formEdit.addEventListener('submit', (event) => {
+    event.preventDefault();
+    formValidation();
+});
 
+let formValidation = () => {
 
-// let formValidation = () => {
-
-//   msg.classList.add("d-none");
-//   msg1.classList.add("d-none");
-//   msg2.classList.add("d-none");
+  msg.classList.add("d-none");
+  msg1.classList.add("d-none");
+  msg2.classList.add("d-none");
   
-//   if (nombre.value === "") {
-//     msg.classList.remove("d-none");
-//   }
-//   if(prefijo.value === ""){
-//     msg1.classList.remove("d-none");
-//   }
-//   if(urlLogo.value === ""){
-//     msg2.classList.remove("d-none");
-//   }
+  if (nombre.value === "") {
+    msg.classList.remove("d-none");
+  }
+  if(prefijo.value === ""){
+    msg1.classList.remove("d-none");
+  }
+  if(urlLogo.value === ""){
+    msg2.classList.remove("d-none");
+  }
 
-//   if (nombre.value !== "" && prefijo.value !== "" && urlLogo.value !== ""){
-//     msg.classList.add("d-none");
-//     msg1.classList.add("d-none");
-//     msg2.classList.add("d-none");
-//     acceptData();
-//   }
-// };
+  if (nombre.value !== "" && prefijo.value !== "" && urlLogo.value !== ""){
+    msg.classList.add("d-none");
+    msg1.classList.add("d-none");
+    msg2.classList.add("d-none");
+    acceptData();
+  }
+};
 
-// async function acceptData(){
-//     const data = {
-//         name: nombre.value,
-//         description: prefijo.value,
-//         logo: urlLogo.value,
-//     }
-//     let token = JSON.parse(localStorage.getItem("authTokens"));
-//     console.log(token.access);
-//     await fetch(BASE_URL+"api/v2/services/", {
-//         method: "PUT",
-//         mode: "cors",
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'Authorization': "Bearer "+token.access
-//           },
-//         body: JSON.stringify(data)
-//     }).then((response)=>{
-//         if (response.ok){
-//             Swal.fire(
-//                 '¡Actualizado!',
-//                 'Los datos se guardaron correctamente',
-//                 'success'
-//               ).then((result) => {
-//                 if (result.isConfirmed) {
-//                     returnTodo();
-//                 }
-//             }) 
-//         }
-//         else{
-//             Swal.fire({
-//                 icon:"error",
-//                 title: 'Oops...',
-//                 text: "¡Ocurrió un error!"
-//             })           
-//         }
-//     })
-// }
-
-// async function setData(){
-//     try {
-//         const response = await fetch(BASE_URL+"api/v2/services/");
-//         const data = await response.json();
-//         title.value = data.title;
-//         body.value = data.body;
-//         statusTodo.value = data.status;
-//       } catch (error) {
-//         console.log(error);
-//       }
-// }
-
-// function returnTodo(){
-//     window.location.replace(`./detail.html?id=${id}`);
-// }
-
-// setData();
+async function acceptData(){
+    const data = {
+        name: nombre.value,
+        description: prefijo.value,
+        logo: urlLogo.value,
+    }
+    let token = JSON.parse(localStorage.getItem("authTokens"));
+    console.log(token.access);
+    let id = servicio.value
+    await fetch(BASE_URL+"api/v2/services/"+id+'/', {
+        method: "PUT",
+        mode: "cors",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer "+token.access
+          },
+        body: JSON.stringify(data)
+    }).then((response)=>{
+        if (response.ok){
+            Swal.fire(
+                '¡Actualizado!',
+                'Los datos se guardaron correctamente',
+                'success'
+              ).then((result) => {
+                if (result.isConfirmed) {
+                  window.location.replace("./index.html");
+                }
+            }) 
+        }
+        else{
+            Swal.fire({
+                icon:"error",
+                title: 'Oops...',
+                text: "¡Ocurrió un error!"
+            })           
+        }
+    })
+}
